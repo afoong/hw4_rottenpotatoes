4ff225231bc5e139c4f55952fbb7850661df5f36
afoong <a.j.foong@gmail.com>
2012-03-29 00:13:43 -0700

    all tests passing

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rottenpotatoes::Application.routes.draw do
   #   resources :products
   resources :movies
 
+  match "/movies(/sameDirector(/:id))" => "movies#sameDirector", :as => :sameDirector_movies
+
   # Sample resource route with options:
   #   resources :products do
   #     member do
